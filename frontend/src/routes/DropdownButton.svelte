14bbad427139abb04c1e3f8889e97444b5735be7
f-escarate <felipeescaratefernandez@gmail.com>
2023-10-15 12:58:00 -0300

    feat: init svelte app

---
+++ frontend/src/routes/DropdownButton.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { Dropdown, DropdownItem, DropdownDivider, Button } from 'flowbite-svelte';
+    import { ChevronDownOutline } from 'flowbite-svelte-icons';
+    var buttonStyle = 'text-3xl transition-transform hover:scale-125 my-auto px-2'
+    export var title = 'Button';
+    export var values = [
+        {name: 'Home', href: ''},
+        {name: 'About', href: 'about'},
+        {name: 'Contact', href: 'contact'}
+    ];
+
+</script>
+
+
+<main>
+    <Button class={buttonStyle} pill>
+        {title}<ChevronDownOutline class="w-3 h-3 ml-2 text-primary-800 dark:text-white inline" />
+    </Button>    
+    <Dropdown class="w-44 z-20">
+        {#each values as element, i}
+        <DropdownItem href={`?/${element.href}`}>{element.name}</DropdownItem>
+        {/each}
+    </Dropdown>
+</main>
---
+++ frontend/src/routes/Navbar.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { HomeSolid } from 'flowbite-svelte-icons';
+    import DropdownButton from './DropdownButton.svelte';
+
+    let webDeb = [
+        {name: 'Django', href: 'django'},
+        {name: 'Svelte', href: 'svelte'},
+        {name: 'React', href: 'react'}
+    ];
+    let games = [
+        {name: 'Frog', href: 'frog'},
+        {name: 'Ship', href: 'ship'},
+        {name: 'AOne', href: 'aone'}
+    ];
+    let iot = [
+        {name: 'Arduino', href: 'arduino'},
+        {name: 'Raspberry Pi', href: 'raspberry-pi'},
+        {name: 'ESP32', href: 'esp32'}
+    ]
+    
+</script>
+
+<main>
+    <div class='px-5 py-0 flex h-[12vh] bg-blue-200'>
+        <a class="m-2 w-10 h-10 text-primary-800 dark:text-white inline self-center" href="/">
+            <HomeSolid class='w-full h-full'/>
+        </a>
+        <div class='w-1/2 mx-auto grid grid-cols-3 items-center justify-items-center space-x-4'>
+            <DropdownButton title="WebDeb" values={webDeb}/>
+            <DropdownButton title="Videogames" values={games}/>
+            <DropdownButton title="IoT" values={iot}/>
+        </div>
+    </div>
+</main>
