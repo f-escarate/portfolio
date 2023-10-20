78f2779ccaa75dc6e3f6eae6ea4097b5ffdd7903
Felipe Escárate <felipe.escarate@ug.uchile.cl>
2023-10-19 22:55:48 -0300

    feat: add Header

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
+    import Header from './Header.svelte';
     import Navbar from './Navbar.svelte';
     import Sidebar from './Sidebar.svelte'
     import data from '../data/data.json';
     var home_post = data.home;
 </script>
 
-<main class='h-screen bg-blue-100'>
-    <Navbar css_class='h-[12vh]'/>
-    <div class='grid grid-flow-col auto-cols-max '>
+<main class='h-full bg-blue-100 font-mono scroll-smooth'>
+    <Header></Header>
+    <Navbar css_class='h-[12vh] relative'/>
+    <div class='grid grid-flow-col auto-cols-max relative'>
         <Sidebar css_class='h-[88vh]'/>
         <div class='p-4 mx-10 my-5'>
             <h1 class='text-4xl font-bold'>{home_post.title}</h1>
---
+++ frontend/src/routes/Header.svelte
@@ -0,0 +1,34 @@
+<script>
+    import GithubSolid from '../lib/icons/GithubSolid.svelte';
+    import LinkedinSolid from '../lib/icons/LinkedinSolid.svelte';
+    import AddressBookSolid from '../lib/icons/AddressBookSolid.svelte';
+</script>
+
+<main>
+    <div class='h-[80vh] grid place-content-center pr-[300px]'>
+        <div class='h-[90%] p-2 flex flex-wrap'>
+            <div class='h-full m-4 p-2 grid place-content-center'>
+                <h1 class='m-2 p-1 text-6xl'>Felipe Escárate</h1>
+                <h2 class='m-2 p-1 text-l color-slate-500'>Estudiante de 5to año de Ingeniería Civil en Computación</h2>
+            </div>
+
+            <div class='h-full m-4 ml-24 p-2 flex flex-wrap flex-col'>
+                <div class='bg-slate-500 rounded-full m-1 p-1 h-10 aspect-square'>
+                    <a class='grid place-items-center h-full w-full'href="https://github.com/f-escarate" target="_blank" rel="noopener noreferrer">
+                        <GithubSolid css_class='w-2/3 h-2/3 text-blue-100 transition duration-75 group-hover:text-gray-900' />
+                    </a>
+                </div>
+                <div class='bg-slate-500 rounded-full m-1 p-1 h-10 aspect-square'>
+                    <a class='grid place-items-center h-full w-full'href="https://www.linkedin.com/in/felipe-esc%C3%A1rate-793380152/" target="_blank" rel="noopener noreferrer">
+                        <LinkedinSolid css_class='w-2/3 h-2/3 text-blue-100 transition duration-75 group-hover:text-gray-900' />
+                    </a>
+                </div>
+                <div class='bg-slate-500 rounded-full m-1 p-1 h-10 aspect-square'>
+                    <a class='grid place-items-center h-full w-full'href="" target="_blank" rel="noopener noreferrer">
+                        <AddressBookSolid css_class='w-2/3 h-2/3 text-blue-100 transition duration-75 group-hover:text-gray-900' />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
