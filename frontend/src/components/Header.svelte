0145e735cbc39b20e0e5afc704cdda0755a15c02
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-07-26 22:01:41 -0400

    change: header styles

--- frontend/src/components/Header.svelte
+++ frontend/src/components/Header.svelte
@@ -11,27 +11,29 @@
     }
 </script>
 
-    <div class='md:h-[90vh] max-w-[900px] mx-auto flex justify-between items-center'>
-        <div class='m-4 p-2 flex flex-col'>
-            <h1 class='m-2 p-1 text-6xl'>Felipe Escárate F.</h1>
-            <h2 class='m-2 p-1 text-lg color-slate-500'>Estudiante de 5to año de Ingeniería Civil en Computación</h2>
+<div class='md:h-[90vh] max-w-[900px] m-auto flex justify-center items-center'>
+    <div class='flex w-full justify-between items-stretch'>
+        <div class='m-4 p-2 flex flex-col justify-between min-h-full'>
+            <h1 class='text-7xl'>Felipe Escárate F.</h1>
+            <h2 class='text-2xl text-slate-800'>Estudiante de 5to año de Ingeniería Civil en Computación</h2>
         </div>
 
-        <div class='m-4 ml-8 p-2 flex flex-wrap flex-col'>
-            <div class='bg-blue-500 rounded-full m-1 p-1 h-10 aspect-square'>
+        <div class='m-2 p-1 flex flex-wrap flex-col h-max'>
+            <div class='bg-blue-500 hover:bg-cyan-400 rounded-full m-1 p-1 h-10 aspect-square'>
                 <a class='grid place-items-center h-full w-full'href="https://github.com/f-escarate" target="_blank" rel="noopener noreferrer">
                     <GithubSolid css_class='w-2/3 h-2/3 text-blue-100 transition duration-75 group-hover:text-gray-900' />
                 </a>
             </div>
-            <div class='bg-blue-500 rounded-full m-1 p-1 h-10 aspect-square'>
+            <div class='bg-blue-500 hover:bg-cyan-400 rounded-full m-1 p-1 h-10 aspect-square'>
                 <a class='grid place-items-center h-full w-full'href="https://www.linkedin.com/in/felipe-esc%C3%A1rate-793380152/" target="_blank" rel="noopener noreferrer">
                     <LinkedinSolid css_class='w-2/3 h-2/3 text-blue-100 transition duration-75 group-hover:text-gray-900' />
                 </a>
             </div>
-            <div class='bg-blue-500 rounded-full m-1 p-1 h-10 aspect-square'>
+            <div class='bg-blue-500 hover:bg-cyan-400 rounded-full m-1 p-1 h-10 aspect-square'>
                 <button class='grid place-items-center h-full w-full' on:click={handleClickContact}>
                     <AddressBookSolid css_class='w-2/3 h-2/3 text-blue-100 transition duration-75 group-hover:text-gray-900' />
                 </button>
             </div>
         </div>
     </div>
+</div>
