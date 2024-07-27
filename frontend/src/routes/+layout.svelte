6e2434221854a9b0ffb52efafc89000651654abc
Felipe Escárate <felipeescaratefernandez@gmail.com>
2024-07-26 22:05:48 -0400

    fix: width

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,7 +4,7 @@
     import Header from '../components/Header.svelte';
     import Navbar from '../components/Navbar.svelte';
 </script>
-<div class='relative mb-0 bg-blue-100 font-roboto scroll-smooth'>
+<div class='relative mb-0 bg-blue-100 font-roboto scroll-smooth w-full'>
     <Header/>
     <Navbar/>
     <slot />
