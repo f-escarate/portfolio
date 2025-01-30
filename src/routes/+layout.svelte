2b401b9edcfff487b6553e6ee63b4bea5292ca18
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-01-29 22:55:26 -0300

    feat: change head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,9 @@
     import Header from '../components/Header.svelte';
     import Navbar from '../components/Navbar.svelte';
 </script>
+<svelte:head>
+    <title>Portafolio Felipe Escárate F.</title>
+</svelte:head>
 <div class='relative mb-0 bg-blue-100 font-roboto scroll-smooth w-full'>
     <Header/>
     <Navbar/>
