e3cbe79c5c352c3cd3b0edccba3930ce2e6da757
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-02-11 18:16:23 -0300

    refactor: load data.json once

--- src/components/Section.svelte
+++ src/components/Section.svelte
@@ -1,22 +1,21 @@
 <script>
-    import data from '../data/data.json'
     import Description from './Description/Description.svelte';
     import SectionElement from './SectionItem.svelte';
     import Timeline from './Timeline/Timeline.svelte';
-    export var name = '';
-    let elements = data[name].elements
+    export var id = '';
+    export var data;
 </script>
 
 
 <div class='w-5/6 lg:w-4/6 mt-10 md:mb-10 max-w-[900px]'>
-    <h1 id={name} class='mb-4 md:mb-7 lg:mb-10'> {data[name].title} </h1>
-    {#if data[name].type === 'DescriptionSection'}
-        <Description data={data[name]}/>
-    {:else if  data[name].type === 'GroupsSection'}
-        {#each elements as element}
+    <h1 id={id} class='mb-4 md:mb-7 lg:mb-10'> {data.title} </h1>
+    {#if data.type === 'DescriptionSection'}
+        <Description data={data}/>
+    {:else if  data.type === 'GroupsSection'}
+        {#each data.elements as element}
             <SectionElement element={element}/>
         {/each}
-    {:else if data[name].type === 'TimelineSection'}
-        <Timeline elements={data[name].elements}/>
+    {:else if data.type === 'TimelineSection'}
+        <Timeline elements={data.elements}/>
     {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import Section from '../components/Section.svelte';
+    import data from '../data/data.json'
 </script>
 
 <div class='flex flex-col items-center w-[100%]'>
-    <Section name='experience'></Section>
-    <Section name='projects'></Section>
-    <Section name='games'></Section>
+    <Section id='experience' data={data['experience']}/>
+    <Section id='projects'   data={data['projects']}/>
+    <Section id='games'      data={data['games']}/>
     <!-- <Section name='ML'></Section> -->
 </div>
