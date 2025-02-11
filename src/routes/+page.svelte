f3deee0f232ce548f165811cceb398c41e91b5fa
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-02-11 18:18:34 -0300

    change: separate experience to its own json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import Section from '../components/Section.svelte';
     import data from '../data/data.json'
+    import experience from '../data/experience.json'
 </script>
 
 <div class='flex flex-col items-center w-[100%]'>
-    <Section id='experience' data={data['experience']}/>
+    <Section id='experience' data={experience}/>
     <Section id='projects'   data={data['projects']}/>
     <Section id='games'      data={data['games']}/>
     <!-- <Section name='ML'></Section> -->
