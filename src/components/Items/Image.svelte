1ce78d59c7ecc961768afbebc0821a2a523884f8
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-02-12 00:16:13 -0300

    feat; add image component

---
+++ src/components/Items/Image.svelte
@@ -0,0 +1,6 @@
+<script>
+    export let data;
+</script>
+
+<img src={data.src} alt={data.alt} class='md:max-w-[33%] rounded-3xl object-contain float-left mx-6 hover:opacity-90 hover:scale-95 duration-300'/>
+
--- src/components/Items/MultimediaItem.svelte
+++ src/components/Items/MultimediaItem.svelte
@@ -1,15 +1,14 @@
 <script>
     import Description from "../Description/Description.svelte";
+    import Image from "./Image.svelte";
     export let data;
     export let reverse = false;
     let direction = reverse? 'flex-row-reverse':'flex-row';
     let mediaType = data.mediaType;
 </script>
-<div class={`py-6 mb-2 flex flex-col md:flex-row gap-4 ${direction}`}>
+<div class={`py-6 mb-2 flex flex-col md:flex-row gap-2 items-center ${direction}`}>
     {#if mediaType === 'image'}
-        <img src={data.src} alt={data.alt} class='md:w-1/2'/>
+        <Image data={data} />
     {/if}
-    <div class="md:w-1/2">
-        <Description data={data.descriptionData} />
-    </div>
+    <Description data={data.descriptionData} />
 </div>
