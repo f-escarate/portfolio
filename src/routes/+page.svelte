8a4c72cee3cc9bc230108ace279a0fc8d94a99ef
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-03-10 10:29:37 -0300

    change: merge layout.svelte and page.svelte in order to have more control over the layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,44 @@
 <script>
+    import { Modal } from "flowbite-svelte";
+    import "../app.css";
+    import Footer from "../components/Footer.svelte";
+    import Header from '../components/Header.svelte';
+    import Navbar from '../components/Navbar.svelte';
+    import { modalData } from "../stores.js";
+    import Character from "../components/Character.svelte";
     import Section from '../components/Section.svelte';
     import data from '../data/data.json'
     import experience from '../data/experience.json'
     import education from '../data/education.json'
-</script>
+    
+    let modalContent = null;
+    let modalVisible = false;
+    modalData.subscribe(({open, content}) => {modalVisible = open; modalContent = content;});
+    $: modalVisible, modalData.set({
+        open: modalVisible,
+        content: modalContent
+    });
 
-<div class='flex flex-col px-4 lg:p-0 items-start lg:items-center w-[100%]'>
-    <Section id='experience' data={experience}/>
-    <Section id='education'  data={education}/>
-    <Section id='projects'   data={data['projects']}/>
-    <Section id='games'      data={data['games']}/>
-    <!-- <Section name='ML'></Section> -->
+</script>
+<svelte:head>
+    <title>Portafolio Felipe Escárate F.</title>
+</svelte:head>
+<div class='relative mb-0 bg-pale font-roboto scroll-smooth w-full'>
+    <Header/>
+    <Navbar/>
+    <div class='flex flex-col px-4 lg:p-0 items-start lg:items-center w-[100%]'>
+        <Section id='experience' data={experience}/>
+        <Section id='education'  data={education}/>
+        <Section id='projects'   data={data['projects']}/>
+        <Section id='games'      data={data['games']}/>
+        <!-- <Section name='ML'></Section> -->
+    </div>
+    <Footer/>
+
+    <Character />
+    <Modal bind:open={modalVisible} autoclose outsideclose>
+        {#if modalContent}
+        <img src={modalContent.src} alt={modalContent.alt} class='rounded-3xl object-contain min-h-[70vh] max-h-[70vh] mx-auto'/>
+        {/if}
+    </Modal>
 </div>
