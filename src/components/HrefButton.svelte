6635067db8ad1ea9ab0ce42d1cf1c946220e200b
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-03-10 11:12:45 -0300

    refactor: make href button accept url and css classes

--- src/components/HrefButton.svelte
+++ src/components/HrefButton.svelte
@@ -1,21 +1,27 @@
 <script>
     export var title = 'Button';
+    export var href = '';
     export var element_id = '';
+    export var buttonClass = '';
+    export var pClass = '';
 
-    function handleClick() {
+    const handleClick = element_id?
+        () => {
         let el = document.getElementById(element_id);
         window.scrollTo({
 			top: el.offsetTop-el.offsetHeight,
 			behavior: 'smooth'
-		})
-    }
+		})}:
+        () => {
+            window.location.href = href;
+        };
 </script>
 
 
 <button 
-    class='h-max py-3 rounded-lg transition-transform bg-light hover:bg-light-hover hover:scale-110 lg:px-4 bg-opacity-75'
+    class={`h-max py-3 rounded-lg transition-transform bg-light hover:bg-light-hover hover:scale-110 lg:px-4 bg-opacity-75 ${buttonClass}`}
     on:click={handleClick}>
-    <p class='text-3xl text-black font-semibold leading-none'>
+    <p class={`text-3xl text-black font-semibold leading-none ${pClass}`}>
         {title}
     </p>
     
