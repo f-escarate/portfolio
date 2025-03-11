08570015b1149de2a7d040043cd68ce89c52f462
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-03-11 01:18:00 -0300

    refactor: remove medals component

--- src/routes/(pages)/page1/+page.svelte
+++ src/routes/(pages)/page1/+page.svelte
@@ -1,12 +1,19 @@
 <script>
     import Fabric from './components/Fabric.svelte';
-    import Medals from './components/Medals.svelte';
     import Mission from './components/Mission.svelte';
 </script>
 <div class="flex flex-col">
     <Fabric />
     <Mission />
-    <Medals />
+    <div class="bg-gray-100 w-full p-5 md:py-20">
+        <div class="flex items-center justify-between mx-auto md:max-w-[85%] ">
+            
+            <img src="https://static.wixstatic.com/media/cb0452_0789b1c9bed0431fa18241f7bd61a1fa~mv2.jpg/v1/fill/w_309,h_60,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ACT%20-%20Logo%20White%20(Final)-04.jpg" alt="ACT - Logo White (Final)-04.jpg" style="width: 247px; height: 48px; object-fit: cover; object-position: 50% 50%;" width="247" height="48" loading="lazy" data-ssr-src-done="true" fetchpriority="high">
+            <img src="https://static.wixstatic.com/media/cb0452_ff3867b91dc84e8e829109d6b4908eb7~mv2.png/v1/fill/w_231,h_111,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/cb0452_ff3867b91dc84e8e829109d6b4908eb7~mv2.png" alt="" style="width: 185px; height: 89px; object-fit: cover; object-position: 50% 50%;" width="185" height="89" loading="lazy" data-ssr-src-done="true" fetchpriority="high">
+            <img src="https://static.wixstatic.com/media/cb0452_e2805cc1127540a399e69bc7532f846c~mv2.png/v1/fill/w_340,h_89,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Outlook-i5ighvtl.png" alt="Outlook-i5ighvtl.png" style="width: 272px; height: 71px; object-fit: cover; object-position: 50% 50%;" width="272" height="71" loading="lazy" data-ssr-src-done="true" fetchpriority="high">
+            <img src="https://static.wixstatic.com/media/cb0452_f22741016e624f51ae7666d4feeb3184~mv2.png/v1/fill/w_159,h_159,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Member%20Badge.png" alt="Member Badge.png" style="width: 127px; height: 127px; object-fit: cover; object-position: 50% 50%;" width="127" height="127" loading="lazy" data-ssr-src-done="true" fetchpriority="high">
+        </div>
+    </div>
     <div class="flex flex-col gap-6 items-start w-[85%] mx-auto py-12">
         <h2 class="text-3xl font-light">press</h2>
         <p class="text-5xl font-bold text-primary pl-5">Read the threads</p>
