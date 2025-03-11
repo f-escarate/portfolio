25ea81f4df3a4e8cb7430678447a31d889424486
Felipe Escárate <felipeescaratefernandez@gmail.com>
2025-03-11 01:10:58 -0300

    feat: add medals component

--- src/routes/(pages)/page1/+page.svelte
+++ src/routes/(pages)/page1/+page.svelte
@@ -1,8 +1,10 @@
 <script>
     import Fabric from './components/Fabric.svelte';
+    import Medals from './components/Medals.svelte';
     import Mission from './components/Mission.svelte';
 </script>
 <div class="flex flex-col">
     <Fabric />
     <Mission />
+    <Medals />
 </div>
